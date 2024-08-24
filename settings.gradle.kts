8c8186ebca4899f88c70f472a2fdb6e960c49a39
Izan <izansr67@gmail.com>
2024-08-24 19:46:00 +0200

    feat: make default module instead a module by version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ arrayOf("api", "plugin").forEach {
 }
 
 // menu adapters
-arrayOf("1_8_R3", "1_16_R3", "1_17_R1", "1_18_R1", "1_18_R2", "1_19_R1", "1_19_R2", "1_19_R3", "1_20_R1", "1_20_R2", "1_20_R3","1_20_6","1_21").forEach {
+arrayOf("1_8_R3", "1_16_R3", "1_17_R1", "1_18_R1", "1_18_R2", "1_19_R1", "1_19_R2", "1_19_R3", "1_20_R1", "1_20_R2", "1_20_R3","default").forEach {
     includePrefixed("menu:adapt:v$it")
 }
 
